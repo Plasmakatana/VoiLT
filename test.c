8ba71e4b8a2d3733ac83e377b1dc5bbb90926b40
Almaas Abbas Rizvi <almaasrizvi1101@gmail.com>
2026-03-21 17:40:11 +0530

    add log system

--- test.c
+++ test.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+#include "log.h"
+
 int main(){
-  printf("this is test\n");
+  log_init("testlog.txt");
+  log_set_level(LOG_ERROR);
+  log_message(LOG_ERROR,"%d %d",100,200);
+  log_access("1.1.1.1", "GET", "/abc/xyz", 200, 500 );
+  log_cleanup();
   return 0;
 }
